36e7619c671cf2780e6899e4dee45e914a50e444
Anzat <leneas@mail.ru>
2024-10-24 15:17:47 +0300

    lesson 88

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import React from 'react';
-
-// my-app->src -> npm start 
-// 13 
+import React, { useState } from 'react';
+//74
 
 
+  function App() {
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+	const [editingIndex, setEditingIndex] = useState(-1);
   
+	const handleClick = (index) => {
+	  setEditingIndex(index);
+	};
   
-function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
+	const handleBlur = (index, value) => {
+	  const newNotes = [...notes];
+	  newNotes[index] = value;
+	  setNotes(newNotes);
+	  setEditingIndex(-1);
+	};
+
+    const handleClickChangeList = () => {
+		let newList = [...notes].sort(function (a, b) {
+			return b-a;
+        });
+		setNotes(newList);
+	};
 	
-	return <div>
-		<p>{str1}</p>
-		<p>{str2}</p>
-	</div>;
-}
 
 
 
-//
+	
+  
+	return (
+	  <div>
+		<ul>
+		  {notes.map((note, index) => (
+			<li key={index}>
+			  {editingIndex === index ? (
+				<input
+				  type="text"
+				  defaultValue={note}
+				  onBlur={(e) => handleBlur(index, e.target.value)}
+				/>
+			  ) : (
+				<span onClick={() => handleClick(index)}>{note}</span>
+			  )}
+			</li>
+		  ))}
+		</ul>
+		<button onClick={() => handleClickChangeList()}>Click to change list</button>
+	  </div>
+	);
+  }
+
 export default App;
